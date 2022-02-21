aa9d4e595ac43bde58202c401e82d934a18f68ac
Claudiu2222 <claupop2002@gmail.com>
2022-02-21 17:28:58 +0200

    PROBLEMA 2

---
+++ main.cpp
@@ -0,0 +1,62 @@
+//PROBLEMA 2
+#include <stdio.h>
+#include <stdlib.h>
+#include<cstring>
+#include<cmath>
+using namespace std;
+int functieStringToNumber(char number[200])
+{
+    int sizeString = strlen(number);
+    int numberUpdated = 0;
+    for(int i = 0; i < sizeString; i++)
+    {
+        numberUpdated += pow(10, sizeString - i - 1) * (number[i] - '0');
+    }
+    return numberUpdated;
+
+}
+bool verificareString( char number[200])
+{
+    for (int i=0; i<strlen(number); i++)
+    {
+        if(isdigit(number[i]) == 0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    FILE* fileProblemaUnu;
+    fileProblemaUnu = fopen("in.txt","r");
+    if(fileProblemaUnu != NULL)
+    {
+        int okk = 0;
+        unsigned sum = 0;
+        char myString[200];
+        while (fgets(myString, 200, fileProblemaUnu))
+        {
+
+            myString[strlen(myString) - 1] = '\0';
+            if(verificareString(myString) == true)
+                sum += functieStringToNumber(myString);
+            else
+            {
+                printf("VALOAREA INSERATA NU ESTE UN NUMAR!");
+                okk = 1;
+                break;
+            }
+        }
+        if(okk == 0)
+            printf("SUMA ESTE : %d",sum);
+
+
+    }
+    else
+    {
+        printf("EROARE DESCHIDERE FISIER");
+    }
+
+
+
+    return 0;
+}
